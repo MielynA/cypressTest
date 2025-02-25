1c5679301b34092c6b471cf8df90328d5f55fe91
mielyn <mielynacosta@pursuit.org>
2025-02-25 17:57:03 -0600

    added negative test scenario

--- cypress/e2e/retail website/register/loginPage.cy.js
+++ cypress/e2e/retail website/register/loginPage.cy.js
@@ -10,4 +10,14 @@ describe("Login user", () => {
     cy.contains("Account Deleted!").should("be.visible");
     cy.clickContinue();
   });
+
+  it("login user with incorrect email and password", () => {
+    cy.clickLoginMenu();
+    cy.get('[data-qa="login-email"]').type("testwrong@example.com");
+    cy.get('[data-qa="login-password"]').type("testWrong");
+    cy.get('[data-qa="login-button"]').click();
+    cy.get("p")
+      .contains("Your email or password is incorrect!")
+      .should("be.visible");
+  });
 });
