2d6bd21321ff0b211a340a95605933a237cea351
mielyn <mielynacosta@pursuit.org>
2025-02-27 16:38:44 -0600

    fix the attach file upload by deleted dup

--- cypress/e2e/retail website/register/loginPage.cy.js
+++ cypress/e2e/retail website/register/loginPage.cy.js
@@ -1,3 +1,5 @@
+import "cypress-file-upload";
+
 describe("Login user", () => {
   beforeEach(() => {
     cy.visit("http://automationexercise.com");
@@ -33,16 +35,5 @@ describe("Login user", () => {
     cy.get('[data-qa="signup-email"]').type("testUsers2272025+3@example.com");
     cy.get('[data-qa="signup-button"]').click();
     cy.contains("Email Address already exist!").should("be.visible");
-  });
-
-  it("should send and upload in contact us form", () => {
-    cy.clickLoginMenu();
-    cy.contains("Contact us").click();
-    cy.get("div.contact-form").contains("Get In Touch").should("be.visible");
-    cy.get('[data-qa="name"]').type("testMie");
-    cy.get('[data-qa="email"]').type("testMie@email.com");
-    cy.get('[data-qa="message"]').type("this is body message test");
-    cy.get('input[type="file"]').attachFile("example.json");
-    cy.submitButton();
   });
 });
