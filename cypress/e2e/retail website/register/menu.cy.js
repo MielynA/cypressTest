4e73840e7dbda6ba3d2fa1505161511209e0f909
mielyn <mielynacosta@pursuit.org>
2025-02-27 16:21:51 -0600

    added new test cases for menu

---
+++ cypress/e2e/retail website/register/menu.cy.js
@@ -0,0 +1,24 @@
+describe("testing menu bar of the automation website", () => {
+  beforeEach(() => {
+    cy.visit("http://automationexercise.com");
+    cy.clickLoginMenu();
+  });
+
+  it("should send and upload in contact us form", () => {
+    cy.contains("Contact us").click();
+    cy.get("div.contact-form").contains("Get In Touch").should("be.visible");
+    cy.get('[data-qa="name"]').type("testMie");
+    cy.get('[data-qa="email"]').type("testMie@email.com");
+    cy.get('[data-qa="message"]').type("this is body message test");
+    cy.get('input[type="file"]').attachFile("example.json");
+    cy.submitButton();
+  });
+
+  it("should verify test cases menu", () => {
+    cy.contains("Test Cases").click();
+    cy.url().should("eq", "https://automationexercise.com/test_cases");
+    cy.get(".panel-group").each(($el) => {
+      cy.wrap($el).click();
+    });
+  });
+});
