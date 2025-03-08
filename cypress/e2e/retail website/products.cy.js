0628ce4ad393b8aeee222f4deb4c358ed4edafc5
mielyn <mielynacosta@pursuit.org>
2025-03-08 13:11:32 -0600

    added test case 9 search all products and validation

--- cypress/e2e/retail website/products.cy.js
+++ cypress/e2e/retail website/products.cy.js
@@ -2,27 +2,32 @@
 
 beforeEach(() => {
   cy.accessUrl();
+  cy.navigateTo("Products", "/products");
+  cy.contains("All Products").should("be.visible");
+
+  //   cy.get("ul.nav a").then(($links) => {
+  //     const randomIndex = Math.floor(Math.random() * $links.length);
+  //     const selectedLink = $links[randomIndex];
+  //     cy.wrap(selectedLink).click();
+  //     cy.url().should("include", selectedLink.getAttribute("href"));
+  //   });
 });
 
 describe("Products Page", () => {
-  it("should verify the products page", () => {
-    cy.navigateTo("Products", "/products");
-    cy.contains("All Products").should("be.visible");
-    // cy.get("ul.nav").contains("a", "View Product").click();
+  it.skip("should verify the products page", () => {
     cy.get("ul.nav a").then(($links) => {
       const randomIndex = Math.floor(Math.random() * $links.length);
       const selectedLink = $links[randomIndex];
       cy.wrap(selectedLink).click();
       cy.url().should("include", selectedLink.getAttribute("href"));
-      cy.get("h2").should("exist").and("be.visible");
-      cy.get("span").should("contain.text", "Rs.");
-      //   cy.get("p").should("contain.text", "Category:");
-      cy.get("p")
-        .invoke("text")
-        .then((text) => {
-          expect(text.trim()).to.not.be.empty; //ensure text is not empty alternative approach
-        });
     });
+    cy.get("h2").should("exist").and("be.visible");
+    cy.get("span").should("contain.text", "Rs.");
+    cy.get("p")
+      .invoke("text")
+      .then((text) => {
+        expect(text.trim()).to.not.be.empty; //ensure text is not empty alternative approach
+      });
     cy.get("p").should(($paragraph) => {
       const text = $paragraph.text();
       expect(text).to.include("Availability");
@@ -35,4 +40,21 @@ describe("Products Page", () => {
         expect($el.text().trim()).to.not.be.empty;
       });
   });
+
+  it("should search the products", () => {
+    // cy.get("#search_product").should("be.visible").type("women"); alternative way
+    cy.fixture("searchQueries").then((data) => {
+      data.queries.forEach((name) => {
+        cy.get("#search_product").clear().type(name);
+      });
+      cy.get(".features_items")
+        .should("exist")
+        .and("contain.text", "All Products")
+        .should("be.visible");
+
+      cy.get(".single-products").should("be.visible");
+    });
+  });
+  //verify the search product by clicking the search icon button
+  //verify the search product by hitting return or enter key
 });
