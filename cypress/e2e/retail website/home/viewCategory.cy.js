1969d0f37bd40637212c06c90627d6e6808e1973
mielyn <mielynacosta@pursuit.org>
2025-03-28 15:49:33 -0500

    clean up

--- cypress/e2e/retail website/home/viewCategory.cy.js
+++ cypress/e2e/retail website/home/viewCategory.cy.js
@@ -8,13 +8,10 @@ describe("Category Products", () => {
     // Open Women category and click the first link
     cy.get(".panel-title").contains("Women").click();
     cy.get("#Women .panel-body a").first().click();
-
     cy.get(".title").should("be.visible").and("contain.text", "Women");
-
     // Open Men category and click first link
     cy.get(".panel-title").contains("Men").click();
     cy.get("#Men .panel-body a").first().click();
-
     cy.get(".title").should("be.visible").and("contain.text", "Men");
   });
 
