263342c29a462c37013aa126281d74f3a50e9815
mielyn <mielynacosta@pursuit.org>
2025-04-02 15:06:43 -0500

    added scenario for tc 25

--- cypress/e2e/retail website/home/scrollPage.cy.js
+++ cypress/e2e/retail website/home/scrollPage.cy.js
@@ -1,13 +1,20 @@
 describe("Verify scroll up and down of the page", () => {
   beforeEach(() => {
     cy.accessUrl();
+    cy.navigateTo("Home", "/").should("exist");
   });
 
   it('should verify the scroll up using "arrow" and scroll down functionality', () => {
-    cy.navigateTo("Home", "/").should("exist");
     cy.scrollTo("bottom");
     cy.get("#scrollUp").should("be.visible").click();
     cy.window().its("scrollY").should("equal", 0);
+    cy.get(".header-middle").should("be.visible");
+  });
+
+  it("should verify scroll up withou arrow button and scroll down functionality", () => {
+    cy.scrollTo("bottom");
+    cy.get(".single-widget").contains("Subscription").should("be.visible");
+    cy.scrollTo("top");
     cy.get(".header-middle").should("be.visible");
   });
 });
