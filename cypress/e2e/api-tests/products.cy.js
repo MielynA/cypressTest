f6def2bfe4c52e69cd45db0d8c4c3fe3a669105a
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2025-04-03 15:38:19 -0500

    Apitest2 (#28)
    * add api test for brandlist and add commands
    
    * update test cases and use the commands api
    
    * clean up

--- cypress/e2e/api-tests/products.cy.js
+++ cypress/e2e/api-tests/products.cy.js
@@ -11,22 +11,6 @@ describe("testing API products and response statuses", () => {
   });
 
   it("should GET the content of the body", () => {
-    // cy.request("GET", "https://automationexercise.com/api/productsList").should(
-    //   (response) => {
-    //     expect(response.status).to.eq(200);
-
-    //     const jsonParse = JSON.parse(response.body);
-    //     expect(jsonParse.products).to.be.an("array").not.to.be.empty;
-
-    //     expect(jsonParse).to.have.property("products");
-
-    //     const firstProduct = jsonParse.products[0];
-
-    //     expect(firstProduct).to.have.property("category");
-    //     expect(firstProduct).to.have.property("price");
-    //     expect(firstProduct).to.have.property("brand");
-    //   }
-    // );
     cy.apiRequest(
       "GET",
       "https://automationexercise.com/api/productsList"
@@ -42,17 +26,6 @@ describe("testing API products and response statuses", () => {
   });
 
   it("should POST to all the product list", () => {
-    // cy.request({
-    //   method: "POST",
-    //   url: "https://automationexercise.com/api/productsList",
-    //   failOnStatusCode: false,
-    // }).then((response) => {
-    //   const bodyResponse = JSON.parse(response.body);
-    //   expect(bodyResponse.responseCode).to.eq(405);
-    //   expect(bodyResponse.message).to.eq(
-    //     "This request method is not supported."
-    //   );
-    // });
     cy.apiRequest(
       "POST",
       "https://automationexercise.com/api/productsList"
