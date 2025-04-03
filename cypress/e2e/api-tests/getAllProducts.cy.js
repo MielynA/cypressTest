9b181de9b52e9f5273f011f4af9482de5a525048
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2025-04-03 13:53:25 -0500

    add cypress api test for get all products (#25)

--- cypress/e2e/api-tests/getAllProducts.cy.js
+++ cypress/e2e/api-tests/getAllProducts.cy.js
@@ -1,9 +1,29 @@
-describe("testing API products and response status to 200", () => {
+describe("testing API products and response statuses", () => {
   it("should test to get the id of the product", () => {
     cy.request("GET", "https://automationexercise.com/api/productsList").should(
       (response) => {
+        console.log(response);
         expect(response.status).to.eq(200);
         expect(response.body).to.have.contains("id", 1);
+      }
+    );
+  });
+
+  it("should GET the content of the body", () => {
+    cy.request("GET", "https://automationexercise.com/api/productsList").should(
+      (response) => {
+        expect(response.status).to.eq(200);
+
+        const jsonParse = JSON.parse(response.body);
+        expect(jsonParse.products).to.be.an("array").not.to.be.empty;
+
+        expect(jsonParse).to.have.property("products");
+
+        const firstProduct = jsonParse.products[0];
+
+        expect(firstProduct).to.have.property("category");
+        expect(firstProduct).to.have.property("price");
+        expect(firstProduct).to.have.property("brand");
       }
     );
   });
