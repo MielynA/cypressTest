aa84b77510f0e3202b9169f2c29a6f4020e93eec
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2025-04-11 12:23:37 -0500

    Api14 get account detail (#37)
    * added scenario for GET method user account detail
    
    * clean up
    
    * clean up

--- cypress/e2e/api-tests/products.cy.js
+++ cypress/e2e/api-tests/products.cy.js
@@ -219,4 +219,20 @@ describe("testing API products and response statuses", () => {
       expect(parseBody.message).to.eq("User updated!");
     });
   });
+
+  it("should GET user account details", () => {
+    const email = "testUsers2272025+2@example.com";
+    const encodedEmail = encodeURIComponent(email);
+    cy.apiRequest(
+      "GET",
+      `https://automationexercise.com/api/getUserDetailByEmail?email=${encodedEmail}`
+    ).then(({ parseBody, status }) => {
+      expect(status).to.eq(200);
+      expect(parseBody.responseCode).to.eq(200);
+      expect(parseBody.user.email).to.eq(email);
+      expect(parseBody.user.first_name).to.eq("Mietest");
+      expect(parseBody.user.name).to.eq("testUsers_2272025");
+      expect(parseBody.user.country).to.eq("United States");
+    });
+  });
 });
